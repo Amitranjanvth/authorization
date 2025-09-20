d4862f10710c6f50afd8384707abe8abb0cfaf28
AmitRanjanvth <bittuqqqq4@gmail.com>
2025-09-20 19:25:13 +0530

    full setup and add database

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieparser from 'cookie-parser';
 import cors from 'cors';
-import {authRouter} from './routes/authRoutes.js'
+
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-app.use(express.json({limit: '10kb'}));
+app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: '10kb'}));
 app.use(express.static('public'));
 app.use(cookieparser());
@@ -18,7 +18,7 @@ app.get('/', function(req,res){
     res.send("welcome")
 })
 
-app.use('/api/auth', authRouter);
+
 
 export default app;
 
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import {registerUser, loginUser, logoutUser} from '../controllers/authController.js'
+import {registerUser, loginUser, logoutUser, sendVerifyOtp, verifyEmail, isAuthenticated, resetOtp, resetPassword } from '../controllers/authController.js'
+import userAuth from '../../middlewares/userAuth.js'
 
 const authRouter = express.Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', logoutUser);
+authRouter.post('/sendverifyotp',userAuth, sendVerifyOtp);
+authRouter.post('/verifyemail',userAuth, verifyEmail);
+authRouter.post('/isauth',userAuth, isAuthenticated);
+authRouter.post('/resetotp', resetOtp);
+authRouter.post('/resetpassword', resetPassword);
 
 export {authRouter};
